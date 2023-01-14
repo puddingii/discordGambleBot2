cb150be8658c60cb078fb5698cf9fd3fc80b5e92
puddingii <gun4930@naver.com>
2023-01-15 03:11:23 +0900

    feat(controller): 주식 매수/매도 REST API 제작 및 변수 변경

--- src/controller/api/userController.ts
+++ src/controller/api/userController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import _ from 'lodash';
+import DataManager from '../../game/DataManager';
 import { IStock } from '../../model/Stock';
-import { IUserInfo } from '../../model/User';
 
 type MyStockInfo = {
 	stockList: Array<{
@@ -15,9 +15,17 @@ type MyStockInfo = {
 		profilMargin: number;
 		holdingRatio?: number;
 	}>;
-	totalMyValue: number;
-	totalStockValue: number;
+	totalAvgValue: number;
+	totalCurrentValue: number;
 };
+
+type PatchStockBodyInfo = {
+	type: 'b' | 's';
+	cnt: number;
+	stockName: string;
+};
+
+const dataManager = DataManager.getInstance();
 
 export const getUserInfo = (req: Request, res: Response) => {
 	const { user } = req;
@@ -27,21 +35,71 @@ export const getUserInfo = (req: Request, res: Response) => {
 	return res.status(200).json(user);
 };
 
+export const patchUserStock = async (req: Request, res: Response) => {
+	try {
+		const { user, body } = req;
+		const { cnt, stockName, type } = body as Partial<PatchStockBodyInfo>;
+		if (!user) {
+			return res
+				.status(401)
+				.json({ message: '유저정보가 없습니다. 다시 로그인 해주세요.' });
+		}
+		if (!cnt || !stockName || !type) {
+			throw Error('데이터 오류');
+		}
+		const userManager = dataManager.get('user');
+		const stockManager = dataManager.get('stock');
+		const userInfo = dataManager.get('user').getUser({ discordId: user.discordId });
+		if (!userInfo) {
+			throw Error('유저정보가 없습니다');
+		}
+
+		const stockInfo = stockManager.getStock('', stockName);
+		if (!stockInfo) {
+			throw Error('주식/코인정보가 없습니다');
+		}
+
+		const reCnt = type === 's' ? -1 * Number(cnt) : Number(cnt);
+		const stockResult = userInfo.updateStock(stockInfo, reCnt, false);
+		await userManager.update({
+			type: 'sm',
+			userInfo,
+			optionalInfo: {
+				name: stockInfo.name,
+				cnt: stockResult.cnt,
+				value: stockResult.value,
+			},
+		});
+		return res.status(200).json({ cnt: stockResult.cnt, value: stockResult.value });
+	} catch (err) {
+		let message = err;
+		if (err instanceof Error) {
+			message = err.message;
+		}
+		return res.status(400).json({ message });
+	}
+};
+
 export const getUserStockList = async (req: Request, res: Response) => {
 	try {
-		let user = req.user as IUserInfo;
+		let user = req.user;
 		if (!user) {
-			return res.status(401);
+			return res
+				.status(401)
+				.json({ message: '유저정보가 없습니다. 다시 로그인 해주세요.' });
 		}
 		user = await user.populate('stockList.stock');
+		if (!user) {
+			return res.status(401).json({ message: '유저DB Error. 운영자에게 문의주세요' });
+		}
 		const myStockList = (
 			user.stockList as { stock: IStock; cnt: number; value: number }[]
 		).reduce(
 			(acc: MyStockInfo, myStock) => {
 				if (myStock.cnt > 0) {
 					const myRatio = _.round((myStock.stock.value / myStock.value) * 100 - 100, 2);
-					acc.totalMyValue += myStock.cnt * myStock.value;
-					acc.totalStockValue += myStock.cnt * myStock.stock.value;
+					acc.totalAvgValue += myStock.cnt * myStock.value;
+					acc.totalCurrentValue += myStock.cnt * myStock.stock.value;
 					const beforeValue = myStock.stock.updHistory.at(-2)?.value ?? 0;
 					let stockBeforeRatio = 0;
 					if (beforeValue) {
@@ -62,11 +120,11 @@ export const getUserStockList = async (req: Request, res: Response) => {
 				}
 				return acc;
 			},
-			{ stockList: [], totalMyValue: 0, totalStockValue: 0 },
+			{ stockList: [], totalAvgValue: 0, totalCurrentValue: 0 },
 		);
 		myStockList.stockList = myStockList.stockList.map(stock => {
 			const holdingRatio = _.round(
-				((stock.cnt * stock.myValue) / myStockList.totalMyValue) * 100,
+				((stock.cnt * stock.myValue) / myStockList.totalAvgValue) * 100,
 				2,
 			);
 			return { ...stock, holdingRatio };
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import { getUserInfo, getUserStockList } from '../controller/api/userController';
+import {
+	getUserInfo,
+	getUserStockList,
+	patchUserStock,
+} from '../controller/api/userController';
 import { isLoggedIn } from '../middlewares/express';
 
 const router = express.Router();
 
 router.get('/stocklist', isLoggedIn, getUserStockList);
+router.patch('/stock', isLoggedIn, patchUserStock);
 router.get('/', isLoggedIn, getUserInfo);
 
 export default router;
