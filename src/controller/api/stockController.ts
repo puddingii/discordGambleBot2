0b31edd88c5d678b2d689c5f5966c2876422a331
puddingii <gun4930@naver.com>
2023-01-17 04:30:50 +0900

    chore(swagger): autogen 테스트 및 Controller 부분에서 query에 대한 설명 적은 후 swagger에 적용되는지 확인

--- src/controller/api/stockController.ts
+++ src/controller/api/stockController.ts
@@ -2,6 +2,12 @@ import { Request, Response } from 'express';
 import stockModel from '../../model/Stock';
 
 export const getStockInfo = async (req: Request, res: Response) => {
+	/* #swagger.parameters['name'] = {
+        in: 'query',
+        description: '주식이름',
+        required: true,
+        type: 'string'
+} */
 	const {
 		query: { name },
 	} = req;
@@ -11,8 +17,12 @@ export const getStockInfo = async (req: Request, res: Response) => {
 			throw Error('이름에 해당하는 주식정보가 없습니다.');
 		}
 		return res.status(200).json(stockInfo);
-	} catch (e) {
-		return res.status(400);
+	} catch (err) {
+		let message = err;
+		if (err instanceof Error) {
+			message = err.message;
+		}
+		return res.status(400).json({ message });
 	}
 };
 
