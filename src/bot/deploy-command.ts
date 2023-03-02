f874d589622a6cc3777907538c12113a68cfcbe9
puddingii <gun4930@naver.com>
2023-03-02 14:32:07 +0900

    chore(folder): bot관련 소스파일 옮김

--- src/bot/deploy-command.ts
+++ src/bot/deploy-command.ts
@@ -1,13 +1,14 @@
+import 'reflect-metadata';
 import { REST } from '@discordjs/rest';
 import { RESTPostAPIApplicationCommandsJSONBody, Routes } from 'discord.js';
 
 import dotenv from 'dotenv';
 import path from 'path';
 import fs from 'fs';
-import secretKey from './config/secretKey';
-import { container } from './settings/container';
-import TYPES from './interfaces/containerType';
-import { ILogger } from './util/logger';
+import secretKey from '../config/secretKey';
+import { container } from '../settings/container';
+import TYPES from '../interfaces/containerType';
+import { ILogger } from '../util/logger';
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
@@ -20,13 +21,13 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 	/** Commands defined */
 	const commands: Array<RESTPostAPIApplicationCommandsJSONBody> = [];
-	const commandFolder = fs.readdirSync(path.resolve(__dirname, './bot/commands'));
+	const commandFolder = fs.readdirSync(path.resolve(__dirname, './commands'));
 	const commonCommandFiles = commandFolder.filter(
 		file => file.endsWith('.js') || file.endsWith('.ts'),
 	);
 
 	for await (const file of commonCommandFiles) {
-		const { default: command } = await import(`./bot/commands/${file}`);
+		const { default: command } = await import(`./commands/${file}`);
 		if (command.data) {
 			commands.push(command.data.toJSON());
 		}
@@ -35,15 +36,13 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 	/** Service Folder Init */
 	const detailFolders = commandFolder.filter(file => !file.includes('.'));
 	for await (const folder of detailFolders) {
-		const detailFiles = fs.readdirSync(
-			path.resolve(__dirname, `./bot/commands/${folder}`),
-		);
+		const detailFiles = fs.readdirSync(path.resolve(__dirname, `./commands/${folder}`));
 		const commandFiles = detailFiles.filter(
 			file => file.endsWith('.js') || file.endsWith('.ts'),
 		);
 
 		for await (const file of commandFiles) {
-			const { default: command } = await import(`./bot/commands/${folder}/${file}`);
+			const { default: command } = await import(`./commands/${folder}/${file}`);
 			if (command.data) {
 				commands.push(command.data.toJSON());
 			}
